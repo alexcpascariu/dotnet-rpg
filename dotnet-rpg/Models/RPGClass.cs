777c56a136eea7ed17b3e72ce8628b0171e35c64
alexandru.pascariu <alexandru.pascariu@softvision.ro>
2020-11-22 15:17:28 +0200

    Create Character model

---
+++ dotnet-rpg/Models/RPGClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_rpg.Models
+{
+    public enum RPGClass
+    {
+        Knight = 1,
+        Mage = 2,
+        Cleric = 3
+    }
+}
