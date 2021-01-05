3e7b622f2c84f6939b034d341b8cf4b68bc939da
alexandru.pascariu <alexandru.pascariu@softvision.ro>
2021-01-05 15:59:08 +0200

    Updated character service to work only for current user. Added Weapon model

--- dotnet-rpg/Data/DataContext.cs
+++ dotnet-rpg/Data/DataContext.cs
@@ -9,11 +9,9 @@ namespace dotnet_rpg.Data
 {
     public class DataContext : DbContext
     {
-        public DataContext(DbContextOptions<DataContext> options) : base(options)
-        {
-
-        }
+        public DataContext(DbContextOptions<DataContext> options) : base(options){ }
         public DbSet<Character> Characters { get; set; }
         public DbSet<User> Users { get; set; }
+        public DbSet<Weapon> Weapons { get; set; }
     }
 }
