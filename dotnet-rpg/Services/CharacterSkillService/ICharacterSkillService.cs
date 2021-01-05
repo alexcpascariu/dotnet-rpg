8425cdec527f9bb938fc77ba7c39aa482eaa82cc
alexandru.pascariu <alexandru.pascariu@softvision.ro>
2021-01-05 18:13:16 +0200

    Create CharacterSkillService

---
+++ dotnet-rpg/Services/CharacterSkillService/ICharacterSkillService.cs
@@ -0,0 +1,16 @@
+﻿using dotnet_rpg.Dtos.Character;
+using dotnet_rpg.Dtos.CharacterSkill;
+using dotnet_rpg.Dtos.Skill;
+using dotnet_rpg.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_rpg.Services.CharacterSkillService
+{
+    interface ICharacterSkillService
+    {
+        Task<ServiceResponse<GetCharacterDto>> AddCharacterSkill(AddCharacterSkillDto newCharacterSkill);
+    }
+}
