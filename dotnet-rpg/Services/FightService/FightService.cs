9e1b66ea1a09a4a890116c3f4730dc118e1d9f1c
alexandru.pascariu <alexandru.pascariu@softvision.ro>
2021-01-06 16:14:16 +0200

    Add fighting with skills

--- dotnet-rpg/Services/FightService/FightService.cs
+++ dotnet-rpg/Services/FightService/FightService.cs
@@ -17,7 +17,59 @@ namespace dotnet_rpg.Services.FightService
         {
             _context = context;
         }
-        
+
+        public async Task<ServiceResponse<AttackResultDto>> SkillAttack(SkillAttackDto request)
+        {
+            ServiceResponse<AttackResultDto> response = new ServiceResponse<AttackResultDto>();
+
+            try
+            {
+                Character attacker = await _context.Characters
+                    .Include(c => c.CharacterSkills).ThenInclude(cs => cs.Skill)
+                    .FirstOrDefaultAsync(c => c.Id == request.AttackerId);
+                Character opponent = await _context.Characters
+                    .FirstOrDefaultAsync(c => c.Id == request.OpponentId);
+
+                CharacterSkill characterSkill = attacker.CharacterSkills.FirstOrDefault(cs => cs.Skill.Id == request.SkillId);
+                if (characterSkill is null)
+                {
+                    response.Success = false;
+                    response.Message = $"{attacker.Name} doesn't know that skill.";
+                    return response;
+                }
+
+                int damage = characterSkill.Skill.Damage + (new Random().Next(attacker.Intelligence));
+                damage -= new Random().Next(opponent.Defense);
+                if (damage > 0)
+                {
+                    opponent.Hitpoints -= damage;
+                }
+                if (opponent.Hitpoints <= 0)
+                {
+                    response.Message = $"{opponent.Name} has been defeated!";
+                }
+
+                _context.Characters.Update(opponent);
+                await _context.SaveChangesAsync();
+
+                response.Data = new AttackResultDto
+                {
+                    Attacker = attacker.Name,
+                    AttackerHp = attacker.Hitpoints,
+                    Opponent = opponent.Name,
+                    OpponentHp = opponent.Hitpoints,
+                    Damage = damage
+                };
+            }
+            catch (Exception ex)
+            {
+                response.Success = false;
+                response.Message = ex.Message;
+            }
+
+            return response;
+        }
+
         public async Task<ServiceResponse<AttackResultDto>> WeaponAttack(WeaponAttackDto request)
         {
             ServiceResponse<AttackResultDto> response = new ServiceResponse<AttackResultDto>();
--- dotnet-rpg/Services/FightService/IFightService.cs
+++ dotnet-rpg/Services/FightService/IFightService.cs
@@ -10,5 +10,6 @@ namespace dotnet_rpg.Services.FightService
     public interface IFightService
     {
         Task<ServiceResponse<AttackResultDto>> WeaponAttack(WeaponAttackDto request);
+        Task<ServiceResponse<AttackResultDto>> SkillAttack(SkillAttackDto request);
     }
 }
